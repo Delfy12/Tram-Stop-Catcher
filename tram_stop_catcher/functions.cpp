cc2585bb1dc72c87817e8fbd0a351de14d797f89
Delfy12 <mjastak96@gmail.com>
2022-06-01 21:23:12 +0200

    sfml added

---
+++ tram_stop_catcher/functions.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <headers.hpp>
+
+class object {
+public:
+
+private:
+
+};
--- tram_stop_catcher/main.cpp
+++ tram_stop_catcher/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <headers.hpp>
 
 using namespace std;
 
 int main()
 {
-    cout << "Dobrze Cie widzieć, przyjacielu!" << endl;
-    return 0;
+    sf::RectangleShape rectangle(sf::Vector2f(160.0, 80.0));
+    rectangle.setPosition(200.0, 300.0);
+
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Tram Stop Catcher");
+    while (window.isOpen()) {
+            sf::Event event;
+            while (window.pollEvent(event)) {
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+            window.clear(sf::Color::Black);
+            window.draw(rectangle);
+            window.display();
+        }
+
+        return 0;
 }
